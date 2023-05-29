cc19d3f6efaea0ae0157036a20ebc0463bf86cfa
Arsh Ahmad <123231460+Ahmad-Arsh@users.noreply.github.com>
2023-05-29 12:58:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	 this.name = name;
+  this.age = age;
+}
 
-function Employee(name, age, jobTitle) {}
+// Add greet method to Person prototype
+Person.prototype.greet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+}
+
+function Employee(name, age, jobTitle) {
+	  // Call Person constructor with name and age arguments
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
+
+}
 
 // Do not change code below this line
 window.Person = Person;
